67531ee1343d8474a2594d19102aa28cbce297ea
Pham Tuan Khoi <huynhanhpham734@gmail.com>
2023-06-19 16:15:42 +0700

    register dto

--- src/main/java/com/sell/tea/dtos/auth/RegisterRequest.java
+++ src/main/java/com/sell/tea/dtos/auth/RegisterRequest.java
@@ -1,14 +1,23 @@
 package com.sell.tea.dtos.auth;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class RegisterRequest {
+        @NotBlank
+        @Size(min = 3)
         private String name;
 
+        @NotEmpty
+        @Email
         private String email;
 
+        @NotBlank
         private String password;
 
         private String avatar;
