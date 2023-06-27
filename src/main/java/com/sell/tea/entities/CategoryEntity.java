08c53c9c5c1b715d777d74d55bc8785a98913a8f
Pham Tuan Khoi <huynhanhpham734@gmail.com>
2023-06-27 16:03:15 +0700

    category entity

---
+++ src/main/java/com/sell/tea/entities/CategoryEntity.java
@@ -0,0 +1,32 @@
+package com.sell.tea.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity(name = "category")
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(
+            nullable = false,
+            unique = true
+    )
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<ProductEntity> products = new ArrayList<>();
+
+
+}
